5d275a39cf504dcc0a9086d1d2adb1c6688a614d
Jun Hao <2202348@sit.singaporetech.edu.sg>
2024-07-22 15:49:59 +0800

    Update

--- jenkins/scripts/deploy.sh
+++ jenkins/scripts/deploy.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
 
-set +x
+set -x
 docker run -d -p 80:80 --name my-apache-php-app -v C:\\Users\\User\\Desktop\\SIT\\Year 2\\Trimester 3\\ICT2216_SSD\\jenkins-php-selenium-test\\src:/var/www/html php:7.2-apache
 sleep 1
 set +x
